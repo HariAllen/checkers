2e78b35e54ed6bede28bef0e59965a087c476d9b
Hari Allen <hariallen20@gmail.com>
2020-12-04 12:43:01 -0500

    Given checkers Ids and started on javascript

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
+//Row 1
+const cell1 = document.getElementById(1);
+const cell3 = document.getElementById(3);
+const cell5 = document.getElementById(5);
+const cell7 = document.getElementById(7);
 
+
+//Row 2
+const cell8 = document.getElementById(8);
+const cell10 = document.getElementById(10);
+const cell12 = document.getElementById(12);
+const cell14 = document.getElementById(14);
+
+
+//Row 3
+const cell17 = document.getElementById(17);
+const cell19 = document.getElementById(19);
+const cell21 = document.getElementById(21);
+const cell23 = document.getElementById(23);
+
+
+//Row 4
+const cell24 = document.getElementById(24);
+const cell26 = document.getElementById(26);
+const cell28 = document.getElementById(28);
+const cell30 = document.getElementById(30);
+
+
+//Row 5
+const cell33 = document.getElementById(33);
+const cell35 = document.getElementById(35);
+const cell37 = document.getElementById(37);
+const cell39 = document.getElementById(39);
+
+
+//Row 6
+const cell40 = document.getElementById(40);
+const cell42 = document.getElementById(42);
+const cell44 = document.getElementById(44);
+const cell46 = document.getElementById(46);
+
+
+//Row 7
+const cell49 = document.getElementById(49);
+const cell51 = document.getElementById(51);
+const cell53 = document.getElementById(53);
+const cell55 = document.getElementById(55);
+
+
+//Row 8
+const cell56 = document.getElementById(56);
+const cell58 = document.getElementById(58);
+const cell60 = document.getElementById(60);
+const cell62 = document.getElementById(62);
+
+
+
+
+//Red Checkers
+let rC1 = document.getElementById(Red-1);
+let rC2 = document.getElementById(Red-2);
+let rC3 = document.getElementById(Red-3);
+let rC4 = document.getElementById(Red-4);
+let rC5 = document.getElementById(Red-5);
+let rC6 = document.getElementById(Red-6);
+let rC7 = document.getElementById(Red-7);
+let rC8 = document.getElementById(Red-8);
+let rC9 = document.getElementById(Red-9);
+let rC10 = document.getElementById(Red-10);
+let rC11 = document.getElementById(Red-11);
+let rC12 = document.getElementById(Red-12);
+
+
+
+//Black Checkers
+let bC1 = document.getElementById(Black-1);
+let bC2 = document.getElementById(Black-2);
+let bC3 = document.getElementById(Black-3);
+let bC4 = document.getElementById(Black-4);
+let bC5 = document.getElementById(Black-5);
+let bC6 = document.getElementById(Black-6);
+let bC7 = document.getElementById(Black-7);
+let bC8 = document.getElementById(Black-8);
+let bC9 = document.getElementById(Black-9);
+let bC10 = document.getElementById(Black-10);
+let bC11 = document.getElementById(Black-11);
+let bC12 = document.getElementById(Black-12);
+
+ 
